92772fff9a9fd9ca126cac8c201fd186c83baabb
Ahmed Baig <ahmedsab9912@gmail.com>
2026-07-14 17:13:58 +0500

    Operators question 3

--- oprstate.js
+++ oprstate.js
@@ -13,7 +13,25 @@
 // }
 // console.log(`Its ${evenofffun(67)}`);
 
-// Question 02 
+// Question 02
 //check the number that is entered by the user is multiple of 5 or not .
-let userentnum = 35;
-userentnum %5==0 ? console.log('is not Multiple of 5') : console.log("is Not multiple of");
+// let userentnum = 35;
+// userentnum %5==0 ? console.log('is not Multiple of 5') : console.log("is Not multiple of");
+
+// Question 03
+// Write a code which can give grades to students according to their scores:
+// 80-100 = A, 70-89 = B, 60-69 = C, 50-59 = D, 0-49 = F.
+let studentmarks = 91;
+if (studentmarks >= 90) {
+  console.log("Grade A+");
+} else if (studentmarks >= 80 && studentmarks <= 89) {
+  console.log("Grade A");
+} else if (studentmarks >= 70 && studentmarks <= 79) {
+  console.log("Grade B");
+} else if (studentmarks >= 60 && studentmarks <= 69) {
+  console.log("Grade C");
+} else if (studentmarks >= 50 && studentmarks <= 59) {
+  console.log("Grade D");
+} else {
+  console.log("Grade F");
+}
