ae493c84b14e1e97985587c880f565f9f3ed159b
Ahmed Baig <ahmedsab9912@gmail.com>
2026-07-16 04:20:20 +0500

    loops&Strings question01

--- loops&strings.js
+++ loops&strings.js
@@ -1,1 +1,21 @@
 // Question 01
+// Print all even numbers from 0 to 100.
+// Method 01
+// let init = 0;
+// let endval = 100;
+// for (let i = init; i <= endval; i++) {
+//   console.log(`even number are ${2 * i}`);
+// }
+// console.log("loop ends");
+// Method 02
+// let i = 0;
+// while (i <= 100) {
+//   console.log(`even number are ${2 * i}`);
+//   i++;
+// }
+// Method 03ß
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
